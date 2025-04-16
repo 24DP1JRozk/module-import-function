7a4af5e80c61507aa3ee136be4651dfd3affd981
A242490JR <A242490JR@rvt.lv>
2025-04-16 09:06:51 +0300

    Updated example code

--- import1.py
+++ import1.py
@@ -2,5 +2,5 @@ import random
 
 saraksts = [1,2,3]
 
-random.randint(1,6)
-random.choice(saraksts)
+print(random.randint(1,6))
+print(random.choice(saraksts))
--- import2.py
+++ import2.py
@@ -2,5 +2,5 @@ from random import *
 
 saraksts = [1,2,3]
 
-randint(1,6)
-choice(saraksts)
+print(randint(1,6))
+print(choice(saraksts))
