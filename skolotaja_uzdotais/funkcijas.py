9ee1d268d9a9a4ec74ab2a67ae52716628903803
A242490JR <A242490JR@rvt.lv>
2025-04-23 09:21:14 +0300

    Added some comments

--- skolotaja_uzdotais/funkcijas.py
+++ skolotaja_uzdotais/funkcijas.py
@@ -1,9 +1,12 @@
 def say_word(vards):
+    """Izvada sveicienu ar sev personalizēto vārdu"""
     print("Hello,", vards)
 
 def say_hello():
+    """Izvada sveicienu"""
     print("Hello")
 
 def sum_2_nums(num_a:int, num_b:int):
+    """Izvada summu diviem ievadītajiem veselajiem skaitļiem"""
     sum_nums = num_a + num_b
     print(sum_nums)
