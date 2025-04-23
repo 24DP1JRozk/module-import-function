b6c843efc043efc064a374afbe9fc134c4bcc1ca
A242490JR <A242490JR@rvt.lv>
2025-04-23 09:04:10 +0300

    Added custom files that techer wanted

---
+++ skolotaja_uzdotais/funkcijas.py
@@ -0,0 +1,9 @@
+def say_word(vards):
+    print("Hello,", vards)
+
+def say_hello():
+    print("Hello")
+
+def sum_2_nums(num_a:int, num_b:int):
+    sum_nums = num_a + num_b
+    print(sum_nums)
---
+++ skolotaja_uzdotais/izpilditajs.py
@@ -0,0 +1,7 @@
+from funkcijas import *
+
+say_hello()
+
+say_word("Joels")
+
+sum_2_nums(4, 6)
